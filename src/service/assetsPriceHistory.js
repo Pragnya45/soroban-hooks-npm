41475b41e00f7460ef30a49504eb4edb8d59bda7
Pragnya Sahu <pragnya110@gmail.com>
2025-09-09 12:14:10 +0530

    Bump version to 2.0.9 in package.json

--- src/service/assetsPriceHistory.js
+++ src/service/assetsPriceHistory.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 import urlConfig from "../utils/urlConfig.js";
 
+/**
+ * @typedef {Object} AssetPriceHistoryParams
+ * @property {string} code - Asset code (e.g., XLM)
+ * @property {string} [issuer] - Issuer address (required for non-XLM assets, forbidden for XLM)
+ * @property {number} startTimestamp - Start timestamp in ms
+ * @property {number} endTimestamp - End timestamp in ms
+ * @property {string} [offset] - Resolution (default "1m")
+ */
+
 class AssetsPriceHistory {
     constructor(apiKey) {
         this.apiKey = apiKey;
     }
 
-    async getPriceHistory({ code, issuer, startTimestamp, endTimestamp, offset = "1m" }) {
+    /**
+     * Get asset price history.
+     * @param {AssetPriceHistoryParams} params
+     * @returns {Promise<any>}
+     */
+    async getPriceHistory({
+        code,
+        issuer,
+        startTimestamp,
+        endTimestamp,
+        offset = "1m",
+    }) {
         try {
             if (!code) throw new Error("Asset code is required");
             if (!startTimestamp) throw new Error("startTimestamp is required");
@@ -20,7 +40,6 @@ class AssetsPriceHistory {
                 throw new Error("Issuer must not be provided for native asset XLM");
             }
 
-            // build params dynamically
             const params = {
                 code,
                 startTimestamp,
